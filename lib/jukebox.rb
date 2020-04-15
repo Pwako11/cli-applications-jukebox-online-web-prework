ae91f938e073c0bc57f3ad6ffbcb9c79da698780
Patrick A <akolo@hotmail.com>
2020-04-15 18:24:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,10 +55,12 @@ def exit_jukebox
 end 
 
 def run(songs)
-# puts "Please enter a command:"
-#   input = gets.strip
+  puts "Please enter a command:"
+  input = gets.strip
   
-#   if input == "exit"
-#     exit_jukebox
-#   end 
+  if input == "exit"
+    exit_jukebox
+  end
+  
+  
 end 
